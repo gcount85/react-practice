d66567336e9cd9da1b5c942bb149e6dfb89f38f5
100mgml@gmail.com <100mgml@gmail.com>
2023-01-27 22:05:42 +0900

    Feat: add post layout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 // import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  let [글제목, a] = useState(['여자 구두 추천', '여자 귀걸이 추천', '여자 목걸이 추천'])
+
   return (
     <div className="App">
-      <div>
-        안녕하세요.
+
+      <div className="black-nav">
+        <h4>나의 블로그</h4>
       </div>
 
-    </div>
+      <div className='list'>
+        <h4>{글제목[0]}</h4>
+        <p>1월 27일 발행</p>
+      </div>
+
+      <div className='list'>
+        <h4>{글제목[1]}</h4>
+        <p>1월 27일 발행</p>
+      </div>
+
+      <div className='list'>
+        <h4>{글제목[2]}</h4>
+        <p>1월 27일 발행</p>
+      </div>
+
+
+    </div >
   );
 }
 
